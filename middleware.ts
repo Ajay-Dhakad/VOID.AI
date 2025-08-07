6b494dd7b8caa8bd955ad2d9687e1a909d3b6011
itisAJAY <ajay@sasahyog.com>
2025-08-07 18:11:46 +0530

    feat: enhance middleware logic for authentication route handling and redirection

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,24 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
+const authRoutes = ['/auth/login', '/auth/register', '/auth'];
+
+
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const { pathname } = req.nextUrl;
 
-  console.log("Token:", token);
+  if (token && authRoutes.some(route => pathname.startsWith(route))) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }else if (!token && !authRoutes.some(route => pathname.startsWith(route))) {
+    return NextResponse.redirect(new URL('/auth/login', req.url));
+  }
+
 
 
   return NextResponse.next();
 }
+
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'], // Apply middleware to all routes except API and static files
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
