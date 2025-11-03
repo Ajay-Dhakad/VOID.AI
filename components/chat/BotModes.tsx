fcb709e30dc5763a998d1f6b0b1c375a1d9c1501
Ajay Dhakad <ajaydhakad.py@gmail.com>
2025-11-03 23:43:25 +0530

    fix: streamline model initialization in BotModels component

--- components/chat/BotModes.tsx
+++ components/chat/BotModes.tsx
@@ -1,40 +1,39 @@
-import React, { useEffect } from 'react'
-import { botModels } from './BotModeUtils';
+import React, { useEffect } from "react";
+import { botModels } from "./BotModeUtils";
 
 function BotModels() {
-
   const [model, setModel] = React.useState<string>("");
-  
 
   useEffect(() => {
-
-    const savedModel = localStorage.getItem('model');
-    if (savedModel) {
-      setModel(savedModel);
-    }else{
-      setModel(botModels[0].value);
-      localStorage.setItem('model', botModels[0].value);
-    }
-
-  },[])
+    setModel(botModels[0].value);
+    localStorage.setItem("model", botModels[0].value);
+  }, []);
 
   const handleModelChange = (newModel: string) => {
     setModel(newModel);
-    localStorage.setItem('model', newModel);
+    localStorage.setItem("model", newModel);
   };
 
   return (
     <>
-     <select value={model} onChange={(e) => handleModelChange(e.target.value)} className="p-3 appearance-none hover:w-5 sm:hover:w-[200px] transition-all duration-500 border w-0 sm:w-[150px] bg-slate-50 border-slate-200 dark:border-slate-700 dark:bg-slate-700 rounded-md " name="mode" id="">
-        {botModels?.map((botModel:any) => botModel.active &&
-         ( <option key={botModel.value} value={botModel.value}>
-            {botModel.name}
-          </option>)
+      <select
+        value={model}
+        onChange={(e) => handleModelChange(e.target.value)}
+        className="p-3 appearance-none hover:w-5 sm:hover:w-[200px] transition-all duration-500 border w-0 sm:w-[150px] bg-slate-50 border-slate-200 dark:border-slate-700 dark:bg-slate-700 rounded-md "
+        name="mode"
+        id=""
+      >
+        {botModels?.map(
+          (botModel: any) =>
+            botModel.active && (
+              <option key={botModel.value} value={botModel.value}>
+                {botModel.name}
+              </option>
+            )
         )}
-          </select>
-          
-          </>
-  )
+      </select>
+    </>
+  );
 }
 
-export default BotModels
+export default BotModels;
